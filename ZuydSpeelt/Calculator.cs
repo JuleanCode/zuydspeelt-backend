f83bf3cd1346b6f837fb975dcd06c49c9d7bc2f1
Brandon Wijnands <brandonwijnands@live.nl>
2023-06-02 12:43:47 +0200

    test framework changed to mstest, added calculator class with subtraction and addition

---
+++ ZuydSpeelt/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace ZuydSpeelt
+{
+    public class Calculator
+    {
+        public int addition(int x, int y)
+        {
+            return x + y;
+        }
+        public int subtraction(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
