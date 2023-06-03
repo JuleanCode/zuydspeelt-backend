2ce7ea69bac71d41a646f2cb1f9c2aafbeec4a7b
Brandon Wijnands <brandonwijnands@live.nl>
2023-06-03 15:45:53 +0200

    calculator class removed and removed test cases

--- ZuydSpeeltTests/UnitTest1.cs
+++ ZuydSpeeltTests/UnitTest1.cs
@@ -7,9 +7,9 @@ namespace ZuydSpeeltTests
         [TestMethod]
         public void TestAddition()
         {
-            var calculator = new Calculator();
-            var testout = calculator.addition(20, 20);
-            Assert.AreEqual(40,testout);
+            //var calculator = new Calculator();
+            //var testout = calculator.addition(20, 20);
+            Assert.AreEqual(40,40);
         }
     }
 }
